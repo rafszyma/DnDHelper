8091545bf90d48261d2d6947e7ad29ca6408277a
Rafal <rafalaszymala@gmail.com>
2019-06-25 16:33:08 +0200

    late commit...

--- app/src/main/java/com/example/dndhelper/fragments/SpellbookFragment.java
+++ app/src/main/java/com/example/dndhelper/fragments/SpellbookFragment.java
@@ -1,4 +1,4 @@
-package com.example.dndhelper;
+package com.example.dndhelper.fragments;
 
 import android.os.Bundle;
 import android.support.annotation.NonNull;
@@ -7,53 +7,24 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ListView;
-import android.widget.TextView;
 
+import com.example.dndhelper.R;
 import com.example.dndhelper.adapters.ActiveSpellListAdapter;
-import com.example.dndhelper.character.Character;
-import com.example.dndhelper.enums.SpellSchool;
-
-import java.util.Objects;
 
 public class SpellbookFragment extends Fragment {
 
     // TODO change it to extra spell fragment
-    private static String LEVEL_KEY = "level_key";
-    private static String SPELL_SCHOOL = "spell_school";
-    private View spellView;
-    private ActiveSpellListAdapter adapter;
-    private int spellLevel;
-    private SpellSchool extraSpellSchool;
-
-    public static SpellbookFragment newInstance(int level) {
-        SpellbookFragment spellbookFragment = new SpellbookFragment();
-        Bundle bundle = new Bundle();
-        bundle.putInt(LEVEL_KEY, level);
-        spellbookFragment.setArguments(bundle);
-        return spellbookFragment;
-    }
+    protected View spellView;
+    protected ActiveSpellListAdapter adapter;
 
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
                              @Nullable Bundle savedInstanceState) {
         spellView = inflater.inflate(R.layout.spellbook_fragment, container, false);
-        if (getArguments() != null) {
-            if (getArguments().containsKey(LEVEL_KEY)) {
-                spellLevel = getArguments().getInt(LEVEL_KEY);
+        return InitializeList(spellView);
+    }
 
-                ListView spellList = spellView.findViewById(R.id.activeSpellList);
-                adapter = new ActiveSpellListAdapter(Objects.requireNonNull(getActivity()), Character.getInstance().getSpellbook().getActiveSpells(spellLevel));
-                spellList.setAdapter(adapter);
-                notifyChargesChanged();
-            } else {
-                ListView spellList = spellView.findViewById(R.id.activeSpellList);
-                adapter = new ActiveSpellListAdapter(Objects.requireNonNull(getActivity()), Character.getInstance().getSpellbook().getExtraSpells());
-                spellList.setAdapter(adapter);
-                notifyChargesChanged();
-            }
-        }
-
+    public View InitializeList(View spellView){
         return spellView;
     }
 
@@ -62,8 +33,6 @@ public class SpellbookFragment extends Fragment {
         notifyChargesChanged();
     }
 
-    public void notifyChargesChanged() {
-        TextView spellCharges = spellView.findViewById(R.id.chargesTextView);
-        spellCharges.setText(String.format("%s / %s", Character.getInstance().getSpellbook().getCurrentSpellLevelCharges(spellLevel), Character.getInstance().getSpellbook().getMaxSpellLevelCharges(spellLevel)));
+    public void notifyChargesChanged(){
     }
 }
